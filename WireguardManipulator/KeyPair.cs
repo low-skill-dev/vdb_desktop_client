c4fe1132542f10dae5ba2cb144ebcbfcd5c7b237
Rodion <57228093+LuminoDiode@users.noreply.github.com>
2023-09-30 23:16:14 +0300

    changes

--- WireguardManipulator/KeyPair.cs
+++ WireguardManipulator/KeyPair.cs
@@ -8,10 +8,10 @@ internal sealed class KeyPair
 	private KeyPair(string privKey, string pubKey)
 	{
 		this.Private = privKey.Trim("\r\n\t,; ".ToCharArray());
-		this.Public = pubKey.Trim("\r\n\t,; ".ToCharArray());
+		this.Public = pubKey.Trim("\r\n\t,; ".ToCharArray()); // TODO: WRONG KEY
 	}
 
-	public KeyPair(string privKey) : this(privKey, CommandRunner.Run($"echo {privKey} | wg pubkey")) { }
+	public KeyPair(string privKey) : this(privKey, CommandRunner.Run($"echo {privKey.Trim()} | wg pubkey")) { }
 
 	public KeyPair() : this(CommandRunner.Run($"wg genkey")) { }
 }
